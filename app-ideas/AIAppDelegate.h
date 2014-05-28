6dd80becc5bc8b65dcf77eaf000b5ed017c2e9e1
Jeff Bennion <Jeff@Jeffs-MacBook-Pro.local>
2014-05-27 18:22:39 -0600

    Initial Commit

---
+++ app-ideas/AIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AIAppDelegate.h
+//  app-ideas
+//
+//  Created by Jeff Bennion on 5/27/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ app-ideas/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  app-ideas
+//
+//  Created by Jeff Bennion on 5/27/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AIAppDelegate class]));
+    }
+}
